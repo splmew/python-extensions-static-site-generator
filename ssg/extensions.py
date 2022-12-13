86720471df741770ef73b47af7e909808a065929
splmew <mwozniko@gmail.com>
2022-12-13 15:25:10 +0100

    module1

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -6,7 +6,7 @@ from pathlib import Path
 def load_module(directory, name):
     sys.path.insert(0, directory)
     importlib.import_module(name)
-    sys.path.pop()
+    sys.path.pop(0)
 
 
 def load_directory(directory):
