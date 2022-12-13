8d3113c5380836b83f4d3e51fdca317516b2d23b
splmew <mwozniko@gmail.com>
2022-12-13 15:52:49 +0100

    module3

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -8,7 +8,7 @@ def collect_files(source, site_parsers):
     valid = lambda p: not(isinstance(p, parsers.ResourceParser))
     for path in source.rglob('*'):
         for parser in list(filter(valid, site_parsers)):
-            if parser.valid_file_ext(path):
+            if parser.valid_file_ext(path.suffix):
                 files.append(path)
 
 
